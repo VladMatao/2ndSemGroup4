24dd337639c5768a218da686d8ec7d4dd50bebd9
Vlad Mataoanu <vvlad197@gmail.com>
2017-05-15 10:40:55 +0200

    asd

--- src/DBLayer/DBConnection.java
+++ src/DBLayer/DBConnection.java
@@ -25,29 +25,26 @@ public class DBConnection {
             //load of driver
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             System.out.println("Driver class loaded ok");
-
         }
         catch(Exception e){
             System.out.println("Cannot find the driver");
             System.out.println(e.getMessage());
         }
         try{
-            //connection to the database
             con = DriverManager.getConnection(url);
             con.setAutoCommit(true);
             dma = con.getMetaData(); // get meta data
             System.out.println("Connection to " + dma.getURL());
             System.out.println("Driver " + dma.getDriverName());
             System.out.println("Database product name " + dma.getDatabaseProductName());
-        }//end try
+        }
         catch(Exception e){
             System.out.println("Problems with the connection to the database:");
             System.out.println(e.getMessage());
             System.out.println(url);
-        }//end catch
-    }//end  constructor
+        }
+    }
 
-    //closeDb: closes the connection to the database
     public static void closeConnection()
     {
         try{
@@ -58,19 +55,19 @@ public class DBConnection {
         catch (Exception e){
             System.out.println("Error trying to close the database " +  e.getMessage());
         }
-    }//end closeDB
+    }
 
     //getDBcon: returns the singleton instance of the DB connection
     public java.sql.Connection getDBcon()
     {
         return con;
     }
-    //getDBcon: returns the singleton instance of the DB connection
+
     public static boolean instanceIsNull()
     {
         return (instance == null);
     }
-    //this method is used to get the instance of the connection
+
     public static DBConnection getInstance()
     {
         if (instance == null)
