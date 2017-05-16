9a17aa035598fa596bda0b26e347b3393149723b
Mircea <mircea.odor@gmail.com>
2017-05-16 17:11:06 +0200

    Pisatu asta de gui in care ma fut

--- src/GuiLayer/Menu.java
+++ src/GuiLayer/Menu.java
@@ -3,9 +3,19 @@ package GuiLayer;
 import javax.swing.*;
 
 /**
- Project 2nd Semester Group 4 dmaj0916 UCN
+ * Created by Mircea on 16-May-17.
  */
 public class Menu {
-    private JLabel Description;
-    private JButton button1;
+    private JPanel panel1;
+    private JButton createOrderButton;
+    private JButton manageProductButton;
+    private JButton managerMenuButton;
+    private JButton checkSchduleButton;
+    private JPanel left;
+    private JPanel down;
+    private JPanel center;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
