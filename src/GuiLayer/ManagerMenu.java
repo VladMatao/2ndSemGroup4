3a9536d3059cbc97847b84842c8fffb1b71d0cee
Mircea <mircea.odor@gmail.com>
2017-05-19 09:26:47 +0200

    muie

---
+++ src/GuiLayer/ManagerMenu.java
@@ -0,0 +1,15 @@
+package GuiLayer;
+
+import javax.swing.*;
+
+/**
+ * Created by Mircea on 19-May-17.
+ */
+public class ManagerMenu {
+    private JButton createOrderSupplierButton;
+    private JButton updateOrderButton;
+    private JButton deleteOrderButton;
+    private JButton managePersonButton;
+    private JButton manageCompanyButton;
+    private JButton statisticsButton;
+}
