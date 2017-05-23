ebe5d3292c2b7bc1eab2da2672957ffc37ed7663
ReluM <relu.morosan@gmail.com>
2017-05-23 12:01:46 +0200

    ProductLine

--- src/DBLayer/RawMaterialOrderDBIF.java
+++ src/DBLayer/RawMaterialOrderDBIF.java
@@ -1,7 +1,16 @@
 package DBLayer;
 
+import ModelLayer.RawMaterialOrder;
+
+import java.sql.SQLException;
+import java.util.Date;
+
 /**
- * Created by AsusF550 on 23-May-17.
+ * Project 2nd Semester Group 4 dmaj0916 UCN
  */
-public class RawMaterialOrderDBIF {
+public interface RawMaterialOrderDBIF {
+    void create(String rawMaterialOrderId, String deliveryDate, String orderStatus, double totalPrice, String companyId,String rawMaterialLineId) throws SQLException;
+    RawMaterialOrder read(String orderId) throws SQLException;
+    boolean update(RawMaterialOrder rawMaterialOrder,String RawMaterialOrderId) throws SQLException;
+    boolean delete(String rawMaterialOrderId) throws SQLException;
 }
