2f7ad994b63e9b9b08173157387adca2d1a31003
ReluM <relu.morosan@gmail.com>
2017-05-24 11:16:33 +0200

    ProductLine

--- src/ControlLayer/ManageCompany.java
+++ src/ControlLayer/ManageCompany.java
@@ -1,7 +1,55 @@
 package ControlLayer;
 
+import DBLayer.CompanyDB;
+import ModelLayer.Company;
+
+import java.sql.SQLException;
 /**
- * Created by AsusF550 on 24-May-17.
+ Project 2nd Semester Group 4 dmaj0916 UCN
  */
 public class ManageCompany {
+    private CompanyDB companyDb;
+
+    public ManageCompany() {
+        companyDb = new CompanyDB();
+    }
+
+    public boolean create(String id, String name, String phNr, String email, String companyType, String address){
+        try {
+            companyDb.create(id,name,phNr,email,companyType,address);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    public Company read(String id){
+        Company company = null;
+        try {
+            company = companyDb.read(id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return company;
+    }
+
+    public boolean update(Company company,String id){
+
+        try {
+            return companyDb.update(company,id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean delete(String id){
+        boolean aux = false;
+        try {
+            aux = companyDb.delete(id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return aux;
+    }
 }
