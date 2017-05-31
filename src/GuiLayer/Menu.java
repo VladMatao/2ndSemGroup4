a97ed389e7a3e6878c321c298d64e3ab3704a3be
Mircea <mircea.odor@gmail.com>
2017-05-31 12:59:13 +0200

    muie

--- src/GuiLayer/Menu.java
+++ src/GuiLayer/Menu.java
@@ -14,6 +14,8 @@ import javax.swing.SwingConstants;
 import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.Color;
+import javax.swing.ImageIcon;
 
 public class Menu extends JFrame{
 
@@ -48,14 +50,18 @@ public class Menu extends JFrame{
 	private void initialize() {
 		frmMenu = new JFrame();
 		frmMenu.setTitle("Menu");
-		frmMenu.setBounds(100, 100, 450, 369);
+		frmMenu.setBounds(100, 100, 664, 560);
 		frmMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel panel = new JPanel();
+		panel.setBackground(new Color(25, 93, 115));
+		panel.setForeground(new Color(255, 255, 255));
 		frmMenu.getContentPane().add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
 		
 		JButton createOrderButton = new JButton("Create Order");
+		createOrderButton.setForeground(new Color(255, 255, 255));
+		createOrderButton.setBackground(new Color(2, 52, 68));
 		createOrderButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				frmMenu.dispose();
@@ -63,10 +69,12 @@ public class Menu extends JFrame{
 				productOrderGUI.setVisible(true);
 			}
 		});
-		createOrderButton.setBounds(153, 84, 140, 23);
+		createOrderButton.setBounds(67, 205, 140, 23);
 		panel.add(createOrderButton);
 		
 		JButton productButton = new JButton("Products");
+		productButton.setForeground(new Color(255, 255, 255));
+		productButton.setBackground(new Color(2, 52, 68));
 		productButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				frmMenu.dispose();
@@ -74,25 +82,23 @@ public class Menu extends JFrame{
 				productGUI.setVisible(true);
 			}
 		});
-		productButton.setBounds(153, 154, 140, 23);
+		productButton.setBounds(67, 275, 140, 23);
 		panel.add(productButton);
 		
 		
 		JButton statisticsButton = new JButton("Statistics");
+		statisticsButton.setForeground(new Color(255, 255, 255));
+		statisticsButton.setBackground(new Color(2, 52, 68));
 		statisticsButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
-		statisticsButton.setBounds(153, 188, 140, 23);
+		statisticsButton.setBounds(67, 309, 140, 23);
 		panel.add(statisticsButton);
 		
-		JLabel lblScDumisSrl = new JLabel("SC DUMIS SRL");
-		lblScDumisSrl.setFont(new Font("Times New Roman", Font.BOLD, 26));
-		lblScDumisSrl.setHorizontalAlignment(SwingConstants.CENTER);
-		lblScDumisSrl.setBounds(120, 11, 184, 31);
-		panel.add(lblScDumisSrl);
-		
 		JButton companyButton = new JButton("Company");
+		companyButton.setForeground(new Color(255, 255, 255));
+		companyButton.setBackground(new Color(2, 52, 68));
 		companyButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
@@ -101,18 +107,24 @@ public class Menu extends JFrame{
 				companyGUI.setVisible(true);
 			}
 		});
-		companyButton.setBounds(151, 222, 140, 23);
+		companyButton.setBounds(65, 343, 140, 23);
 		panel.add(companyButton);
 		
 		JButton employeeButton = new JButton("Employee");
-		employeeButton.setBounds(153, 262, 140, 23);
+		employeeButton.setForeground(new Color(255, 255, 255));
+		employeeButton.setBackground(new Color(2, 52, 68));
+		employeeButton.setBounds(67, 383, 140, 23);
 		panel.add(employeeButton);
 		
 		JButton rawMaterialsButton = new JButton("Raw Materials");
-		rawMaterialsButton.setBounds(153, 296, 140, 23);
+		rawMaterialsButton.setForeground(new Color(255, 255, 255));
+		rawMaterialsButton.setBackground(new Color(2, 52, 68));
+		rawMaterialsButton.setBounds(67, 425, 140, 23);
 		panel.add(rawMaterialsButton);
 		
 		JButton ordersButton = new JButton("Orders");
+		ordersButton.setForeground(new Color(255, 255, 255));
+		ordersButton.setBackground(new Color(2, 52, 68));
 		ordersButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				frmMenu.dispose();
@@ -120,7 +132,17 @@ public class Menu extends JFrame{
 				choseOrderGUI.setVisible(true);
 			}
 		});
-		ordersButton.setBounds(153, 118, 140, 23);
+		ordersButton.setBounds(67, 239, 140, 23);
 		panel.add(ordersButton);
+		
+		JLabel label_1 = new JLabel("");
+		label_1.setIcon(new ImageIcon("C:\\Users\\Mircea\\Downloads\\Adidas Shoebox.png"));
+		label_1.setBounds(280, 144, 558, 371);
+		panel.add(label_1);
+		
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon("C:\\Users\\Mircea\\Desktop\\logo2.png"));
+		label.setBounds(-172, 0, 810, 133);
+		panel.add(label);
 	}
 }
