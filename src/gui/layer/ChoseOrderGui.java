b40afecd6182d59d75eecc9bb8450f03f595e962
Mircea <mircea.odor@gmail.com>
2017-06-01 17:01:53 +0200

    choose order type

--- src/gui/layer/ChoseOrderGui.java
+++ src/gui/layer/ChoseOrderGui.java
@@ -2,6 +2,8 @@ package gui.layer;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -16,15 +18,20 @@ public class ChoseOrderGui extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100, 450, 300);
         JPanel contentPane = new JPanel();
+        contentPane.setBackground(new Color(25, 93, 115));
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         setContentPane(contentPane);
         contentPane.setLayout(null);
 
         JButton supplierOrderButton = new JButton("Supplier Order");
-        supplierOrderButton.setBounds(230, 119, 109, 23);
+        supplierOrderButton.setForeground(new Color(255, 255, 255));
+        supplierOrderButton.setBackground(new Color(2, 52, 68));
+        supplierOrderButton.setBounds(248, 147, 109, 23);
         contentPane.add(supplierOrderButton);
         
         JButton btnProductOrders = new JButton("Product Orders");
+        btnProductOrders.setForeground(new Color(255, 255, 255));
+        btnProductOrders.setBackground(new Color(2, 52, 68));
         btnProductOrders.addMouseListener(new MouseAdapter() {
         	@Override
         	public void mouseClicked(MouseEvent e) {
@@ -33,8 +40,13 @@ public class ChoseOrderGui extends JFrame {
         		manageProductOrderGui.setVisible(true);
         	}
         });
-        btnProductOrders.setBounds(66, 119, 89, 23);
+        btnProductOrders.setBounds(67, 147, 109, 23);
         contentPane.add(btnProductOrders);
+        
+        JLabel label = new JLabel("");
+        label.setIcon(new ImageIcon("photos\\chooseordertype.png"));
+        label.setBounds(10, 27, 410, 101);
+        contentPane.add(label);
     }
 
 }
